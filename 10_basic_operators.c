1c430135210919e85bd06164b9b59dfb6a528a20
alanshalem <alanshalem@gmail.com>
2022-08-14 20:54:19 -0300

    fix: add better comments on basic_operators

--- 10_basic_operators.c
+++ 10_basic_operators.c
@@ -55,8 +55,7 @@ int main()
 }
 
 /*
-    Arithmetic, logical, assignment and relational operators
-
+    ? Arithmetic, logical, assignment and relational operators
     An arithmetic operator is a mathematical function that takes two operands and performs a calculation on them
 
     A logical operator (sometimes called a "Boolear opsrator") is an operator that returns a Boolean
@@ -66,6 +65,7 @@ int main()
         assigns the value of the expression at its right to the variable at its left
     A relational operator will compare variables against eachother
 
+    ! Arithmetic operators
     Examples:
     1) + -> adds two operands -> A + B = 30
     2) - -> subtracts second operand from the first -> A - B = -10
@@ -75,13 +75,13 @@ int main()
     6) ++ -> increments operator increases the integer value by 1 -> A++ = 11 (postfix)
     7) -- -> decrements operator decreases the integer value by 1 -> A-- = 9 (postfix)
 
-    Logical Operators
+    ! Logical Operators
     (TRUE = 1, FALSE = 0)
     1) && -> AND operator -> (A && B) is false (if both A and B are true)
     2) || -> OR operator -> (A || B) is true (if either A or B is true)
     3) ! -> NOT operator -> !(A && B) is true (if A is false)
 
-    Assignment Operators
+    ! Assignment Operators
     1) = -> assigns the value of the expression at its right to the variable at its left -> C = A + B
     2) += -> adds the value of the expression at its right to the variable at its left -> C += A is equivalent to C = C + A
     3) -= -> subtracts the value of the expression at its right from the variable at its left -> C -= A is equivalent to C = C - A
@@ -94,7 +94,7 @@ int main()
     10) ^= -> bitwise XOR operator -> C ^= 2 is equivalent to C = C ^ 2
     11) |= -> bitwise OR operator -> C |= 2 is equivalent to C = C | 2
 
-    Relational Operators
+    ! Relational Operators
     1) == -> equal to -> A == B is true if A is equal to B
     2) != -> not equal to -> A != B is true if A is not equal to B
     3) > -> greater than -> A > B is true if A is greater than B
